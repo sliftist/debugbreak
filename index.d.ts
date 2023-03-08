8b3f10d2047890daa952cf79d95d0401922583e9
Quentin Brooks <sliftist@gmail.com>
2023-03-08 10:54:05 -0500

    Continue in browser.

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,22 @@
-export = function (continueOnAttach?: unknown) { };
+/**
+    debugbreak has a lot of features you might not want. Depending on your use case,
+        you can just do this in one (long) line:
+
+        require("open")(`https://notdevtools.com/devtools/inspector.html?experiments=true&v8only=true&${require("inspector").url().replace("://", "=")}`);
+
+    However, if you don't mind the instability of the extra features, you will
+        get better logging, and a slightly easier to use function.
+
+
+    Passing a truthy argument (such as 1) will result in the debugger continuing
+        after attaching. This is useful as it allows you to add a debugger; statement
+        to break in your own code, for example:
+
+        debugbreak(1);
+        debugger;
+
+    @param {boolean} continueOnAttach - Whether to continue after attaching.
+        1 = attach, and then continue
+        2 = attach only once, and then continue
+*/
+export default function (continueOnAttach?: unknown): void;
--- index.js
+++ index.js
@@ -1,32 +1,60 @@
+/**
+        debugbreak has a lot of features you might not want. Depending on your use case,
+            you can just do this in one (long) line:
+  
+            require("open")(`https://notdevtools.com/devtools/inspector.html?experiments=true&v8only=true&${require("inspector").url().replace("://", "=")}`);
+  
+        However, if you don't mind the instability of the extra features, you will
+            get better logging, and a slightly easier to use function.
+
+
+        Passing a truthy argument (such as 1) will result in the debugger continuing
+            after attaching. This is useful as it allows you to add a debugger; statement
+            to break in your own code, for example:
+
+            debugbreak(1);
+            debugger;
+ */
+
 const autoOpen = true;
 module.allowclient = true;
 
-module.exports = function debugBreakpoint(continueOnAttach) {
+let attached = false;
+
+module.exports = function debugbreak(continueOnAttach) {
     if (typeof document !== "undefined") {
+        if (continueOnAttach) return;
         debugger;
         return;
     }
+
+    if (continueOnAttach === 2 && attached) {
+        return;
+    }
+    attached = true;
+
+    // NOTE: Try to avoid just using require, as it might be transpiled or otherwise wrapped.
+    const r = globalThis.require || require;
 
     let baseUrl = baseGetInspectorUrl();
 
     let devToolsUrl = `devtools://devtools/bundled/inspector.html?experiments=true&v8only=true&${baseUrl.replace("://", "=")}`;
 
-    // NOTE: globalThis.require to ensure transpilers don't try to bundle our imports
     // NOTE: writeSync to ensure any async log shims aren't used, as we are about to
     //      stop all code execution, so async logging won't work
-    globalThis.require("fs").writeSync(1, `\nWaiting on\n${devToolsUrl}\n${new Error().stack.split("\n").slice(1).join("\n")}\n`);
+    r("fs").writeSync(1, `\nWaiting on\n${devToolsUrl}\n${new Error().stack.split("\n").slice(1).join("\n")}\n`);
 
     if (autoOpen) {
-        let browserUrl = devToolsUrl.replace("devtools://devtools/bundled", "https://notdevtools.com");
+        let browserUrl = devToolsUrl.replace("devtools://devtools/bundled", "https://notdevtools.com/devtools");
         // Have to exec, or else our waiting for the debugger can/will break the open code.
-        globalThis.require("child_process").exec(`node -e "require('open')('${browserUrl}')"`);
+        r("child_process").exec(`node -e "require('open')('${browserUrl}')"`);
     }
 
     if(!continueOnAttach) {
         // IMPORTANT! Refresh devtools after you continue here! (the code will block until the debugger reattaches)
         debugger;
     }
-    globalThis.require("inspector").waitForDebugger();
+    r("inspector").waitForDebugger();
     if(!continueOnAttach) {
         debugger;
     }
@@ -37,7 +65,7 @@ module.exports = function debugBreakpoint(continueOnAttach) {
             return 49152 + ~~((65535 - 49152) * Math.random());
         }
 
-        let inspector = eval("require")("inspector");
+        let inspector = r("inspector");
 
         while (true) {
             let url = inspector.url();
